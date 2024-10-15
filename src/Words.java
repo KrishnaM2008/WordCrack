bfbc96473759c06acbce53a8d6c5b6046b65a917
KrishnaM2008 <152558201+KrishnaM2008@users.noreply.github.com>
2024-10-14 19:10:55 -0700

    Add files via upload

---
+++ src/Words.java
@@ -0,0 +1,15 @@
+
+public class Words {
+    public String word;
+    
+
+    public Words(String stringWord) {
+        word = stringWord;
+    }
+    
+    public String getWord() {
+        return word;
+    }
+
+
+}
